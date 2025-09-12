699c865ec75ee14f052544dd52a77732ef973f20
Felix Dupuis <felixdupuis233@hotmail.fr>
2025-09-12 16:36:39 -0400

    exercice 1

---
+++ src/TemperatureConversion.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+
+public class TemperatureConversion {
+    private Scanner sc; //creation scanner
+
+    public TemperatureConversion() {
+        sc = new Scanner(System.in);//nom(sc) du scanner
+    }
+    public double demanderCelsius() {
+        System.out.println("Quelle est la température en Celsius ?"); // Question pour celsius
+        return sc.nextDouble(); //retour du user d'un double
+    }
+    public double conversion(double celsius) {
+        return (celsius * 9 / 5) + 32; //calcul conversion
+    }
+    public void reponse (double celsius, double fahrenheit) {
+        System.out.println(celsius + " °C = " + fahrenheit + " °F"); // print la reponse
+    }
+    public void all(){ //tout print
+    double cel = demanderCelsius(); //call de demandeCelsius
+    double fahr = conversion(cel); //call de conversion
+    reponse(cel, fahr);
+        sc.close();
+}
+
+
+
+}
